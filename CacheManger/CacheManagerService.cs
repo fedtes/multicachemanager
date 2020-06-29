8e658ab1aae13d7a5e60b688b8b8f0e89b004909
fedtes <46047532+fedtes@users.noreply.github.com>
2020-06-29 16:48:25 +0200

    init repo

---
+++ CacheManger/CacheManagerService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LightInject;
+
+namespace CacheManager
+{
+    public class CacheManagerService : ICacheProvider
+    {
+
+        private ServiceContainer ServiceContainer;
+
+        private void RootComposition()
+        {
+            
+        }
+        public CacheManagerService()
+        {
+            ServiceContainer = new ServiceContainer();
+            ServiceContainer.RegisterSingleton<ICacheProvider, StandardCacheProvider>();
+            RootComposition();
+        }
+
+        public CacheManagerService(Type cacheprovider)
+        {
+            ServiceContainer = new ServiceContainer();
+            ServiceContainer.RegisterSingleton(typeof(ICacheProvider), cacheprovider);
+            RootComposition();
+        }
+
+        public IDataAccessor GetSlot<T>() where T : BaseSlotType
+        {
+            return ServiceContainer.GetInstance<ICacheProvider>().GetSlot<T>();
+        }
+
+        public void AddSlot<T>() where T : BaseSlotType
+        {
+            ServiceContainer.GetInstance<ICacheProvider>().AddSlot<T>();
+        }
+
+        public void AddSlot(Type slotType)
+        {
+            ServiceContainer.GetInstance<ICacheProvider>().AddSlot(slotType);
+        }
+
+        public void AddSlot(BaseSlotType SlotInstance)
+        {
+            ServiceContainer.GetInstance<ICacheProvider>().AddSlot(SlotInstance);
+        }
+
+
+
+    }
+}
---
+++ CacheManger/SlotType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CacheManager
+{
+    public abstract class BaseSlotType { }
+
+    public abstract class SlotType<T> : BaseSlotType where T : class
+    {
+        public abstract T ValueType { get; }
+        public virtual T DefaultOnMiss { get => null; }
+        public virtual TimeSpan Expiration { get => TimeSpan.FromMinutes(5); }
+        public virtual bool ShouldExpire { get => true; }
+        public virtual Func<CacheItem, CacheItem> OnHit { get => (c) => c; }
+    }
+}
